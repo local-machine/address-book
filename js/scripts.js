c258ad430ec36f31f8ae95300f99e459139fb10c
Jessica Munoz and Na Hyung Choi <na@>
2019-06-18 10:17:03 -0700

    add 2 types of physical address

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = [],
-  this.currentId = 0
+  this.currentId = -1
 }
 
 AddressBook.prototype.addContact = function(contact) {
@@ -38,15 +38,26 @@ AddressBook.prototype.deleteContact = function(id) {
 }
 
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber, emailAddress) {
+function Contact(firstName, lastName, phoneNumber, emailAddress, addressSet) {
   this.firstName = firstName,
   this.lastName = lastName,
   this.phoneNumber = phoneNumber,
-  this.emailAddress = emailAddress
+  this.emailAddress = emailAddress,
+  this.addressSet = addressSet
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
+}
+
+Contact.prototype.addAddress = function(address) {
+  this.addresses.push(address)
+}
+
+// Business Logic for Physical Address ---------
+function AddressSet(homeAddress, workAddress) {
+  this.homeAddress = homeAddress,
+  this.workAddress = workAddress
 }
 
 // User Interface Logic ---------
@@ -63,11 +74,14 @@ function displayContactDetails(addressBookToDisplay) {
 
 function showContact(contactId) {
   var contact = addressBook.findContact(contactId);
+  var newAddressSet = contact.addressSet;
   $("#show-contact").show();
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
   $(".email-address").html(contact.emailAddress);
+  $(".home-address").html(newAddressSet.homeAddress);
+  $(".work-address").html(newAddressSet.workAddress);
   var buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
@@ -83,6 +97,7 @@ function attachContactListeners() {
     displayContactDetails(addressBook);
   });
 };
+
 $(document).ready(function() {
   attachContactListeners();
   $("form#new-contact").submit(function(event) {
@@ -91,11 +106,16 @@ $(document).ready(function() {
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
     var inputtedEmailAddress = $("input#new-email-address").val();
+    var inputtedHomeAddress = $("input#new-home-address").val();
+    var inputtedWorkAddress = $("input#new-work-address").val();
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
     $("input#new-email-address").val("");
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress);
+    $("input#new-home-address").val("");
+    $("input#new-work-address").val("");
+    var newAddressSet = new AddressSet(inputtedHomeAddress, inputtedWorkAddress);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress, newAddressSet);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
   })
